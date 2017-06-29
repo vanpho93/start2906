205086bb0abc54022ed296fd0add0b010e34ae6a
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-29 20:47:30 +0700

    toan tu

---
+++ a.js
@@ -0,0 +1,5 @@
+let a = 'HELLO WORLD!';
+// const b = 'HELLO';
+// var c = 10;
+a = 10; 
+console.log(a);
---
+++ b.js
@@ -0,0 +1,6 @@
+const a = 5;
+const b = '5';
+
+// console.log(a + parseInt(b));
+// console.log(a == b);
+console.log(a === b);
