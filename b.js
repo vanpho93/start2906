11405e54aca4e7731b81387590cf11a7541cb10b
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-29 21:12:53 +0700

    toan tu 3 ngoi

--- b.js
+++ b.js
@@ -3,4 +3,13 @@ const b = '5';
 
 // console.log(a + parseInt(b));
 // console.log(a == b); so sanh 2 gia tri
-console.log(a === b);
+// console.log(a === b);
+
+const c = true;
+const d = false;
+
+console.log(!c);
+console.log(c);
+
+// let e = 10;
+// ++e;
---
+++ c.js
@@ -0,0 +1,5 @@
+const a = 5;
+const b = 10;
+
+const max = a > b ? a : b;
+console.log(max);
